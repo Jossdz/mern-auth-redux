8eb14e2354e2416aa2dc06276277510a8b4dff5d
Jose Carlos Correa <joss@MacBook-Pro-de-Jose-2.local>
2018-09-15 12:31:16 -0500

    materialize css added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom'
 
 class Header extends React.Component{
   render(){
-    return <div>
-      <ul>
-        <li> <Link to='/'>        Home </Link> </li>
-        <li> <Link to='/signup'>  Signup </Link> </li>
-        <li> <Link to='/login'>   Login </Link> </li>
-      </ul>
-    </div>
+    return (
+    <nav>
+      <div className="nav-wrapper">
+        <Link to='/' className="brand-logo">Redux</Link>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li> <Link to='/signup'>  Signup </Link> </li>
+          <li> <Link to='/login'>   Login </Link> </li>
+        </ul>
+      </div>
+    </nav>
+    )
   }
 }
 
--- client/src/routes.js
+++ client/src/routes.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 const Login   = () => <h1> Login </h1>
 const Signup  = () => <h1> Signup </h1>
 const Home    = () => <h1> Home </h1>
+const Private = () => <h1> Private</h1>
 
 export default () =>
   <BrowserRouter>
@@ -13,5 +14,6 @@ export default () =>
       <Route exact path='/' component={Home} />
       <Route path='/login' component={Login}/>
       <Route path='/signup' component={Signup}/>
+      <Route path='/private' component={Private}/>
     </div>
   </BrowserRouter>
